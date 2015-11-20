b1edc8e9d62000ac99da55b26739354a987a25ae
Ricardo Pérez <ricardo@ubuntu.com>
2015-11-20 19:17:47 +0100

    Carga inicial

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Videoclub</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <ul>
+            <li><a href="socios">Gestión de socios</a></li>
+            <li><a href="peliculas">Gestión de películas</a></li>
+            <li><a href="alquileres">Alquileres y devoluciones</a></li>
+        </ul>
+    </body>
+</html>
